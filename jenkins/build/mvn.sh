9f7748db61b0243275d490aa969950731bd20fd2
Krzysztof Gryko <gryko@pc-521.home>
2024-04-06 19:20:50 +0200

    update paths

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 echo "Building the code"
-docker run --rm -v $PWD/java-app:/app -v /root/.m2:/root/.m2/ -w /app maven:3.9.6-eclipse-temurin-17-alpine "$@"
+WORKSPACE=$HOME/jenkins-data/jenkins_home/workspace/pipeline-docker-maven
+
+docker run --rm -v $WORKSPACE/java-app:/app -v /root/.m2:/root/.m2/ -w /app maven:3.9.6-eclipse-temurin-17-alpine "$@"
--- jenkins/test/mvn.sh
+++ jenkins/test/mvn.sh
@@ -2,4 +2,6 @@
 
 echo "Testing the code"
 
-docker run --rm -v $PWD/java-app:/app -v /root/.m2:/root/.m2/ -w /app maven:3.9.6-eclipse-temurin-17-alpine "$@"
+WORKSPACE=$HOME/jenkins-data/jenkins_home/workspace/pipeline-docker-maven
+
+docker run --rm -v $WORKSPACE/java-app:/app -v /root/.m2:/root/.m2/ -w /app maven:3.9.6-eclipse-temurin-17-alpine "$@"
