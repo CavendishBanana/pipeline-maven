e6ca08a772deb7f9b7c365767c514d9573127829
Krzysztof Gryko <gryko@pc-521.home>
2024-04-06 22:14:29 +0200

    fix attempt 2 - add whoami

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -2,5 +2,6 @@
 echo "Building the code"
 WORKSPACE=/home/gryko/jenkins-data/jenkins_home/workspace/pipeline-docker-maven
 echo "WORKSPACE: $WORKSPACE"
+whoami
 ls $WORKSPACE/java-app
 docker run --rm -v $WORKSPACE/java-app:/app -v /root/.m2:/root/.m2/ -w /app maven:3.9.6-eclipse-temurin-17-alpine "$@"
