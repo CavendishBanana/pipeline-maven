1ba5ceca4ee19e63fcae709ddf196c3e31b9e4fc
Krzysztof Gryko <gryko@pc-521.home>
2024-04-06 20:26:25 +0200

    add listings to identify error

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 echo "Building the code"
 WORKSPACE=$HOME/jenkins-data/jenkins_home/workspace/pipeline-docker-maven
-
+echo "WORKSPACE: $WORKSPACE"
+ls $WORKSPACE/java-app
 docker run --rm -v $WORKSPACE/java-app:/app -v /root/.m2:/root/.m2/ -w /app maven:3.9.6-eclipse-temurin-17-alpine "$@"
