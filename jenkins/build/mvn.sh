ba51ad52494f09b79092ac5ad249589019481e8f
Krzysztof Gryko <gryko@pc-521.home>
2024-04-07 06:56:51 +0200

    fix attempt - add pwd to mvn.sh

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -5,5 +5,5 @@ WORKSPACE="/var/jenkins_home/workspace/pipeline-docker-maven"
 echo "WORKSPACE: $WORKSPACE"
 #this is some comment but modified 
 whoami
-ls $WORKSPACE/java-app
+echo "working directory: $(pwd)"
 docker run --rm -v $WORKSPACE/java-app:/app -v /root/.m2:/root/.m2/ -w /app maven:3.9.6-eclipse-temurin-17-alpine "$@"
