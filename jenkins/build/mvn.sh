0dcd427efa8593f62839c357ca69d8666745f530
CavendishBanana <155924297+CavendishBanana@users.noreply.github.com>
2024-04-08 01:18:56 +0200

    Update mvn.sh

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -7,4 +7,5 @@ echo "WORKSPACE: $WORKSPACE"
 #this is some comment but modified 
 whoami
 echo "working directory: $(pwd)"
+ls "$(pwd)"
 docker run --rm -v $WORKSPACE/java-app:/app -v /root/.m2:/root/.m2/ -w /app maven:3.9.6-eclipse-temurin-17-alpine "$@"
