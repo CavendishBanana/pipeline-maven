2c64f0df9c3ff3d7a38f815e466699e6d4a32b7f
Krzysztof Gryko <gryko@pc-521.home>
2024-04-07 06:33:40 +0200

    aaa

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -3,6 +3,7 @@ echo "Building the code"
 #WORKSPACE="/home/$(whoami)/jenkins-data/jenkins_home/workspace/pipeline-docker-maven"
 WORKSPACE="/var/jenkins_home/workspace/pipeline-docker-maven"
 echo "WORKSPACE: $WORKSPACE"
+#this is some comment
 whoami
 ls $WORKSPACE/java-app
 docker run --rm -v $WORKSPACE/java-app:/app -v /root/.m2:/root/.m2/ -w /app maven:3.9.6-eclipse-temurin-17-alpine "$@"
