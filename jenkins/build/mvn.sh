6b38a9bfeb6dada714f35c3ffb58bdea32a75133
Krzysztof Gryko <gryko@pc-521.home>
2024-04-08 13:29:12 +0200

    back to my workspace

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 echo "Building the code"
-WORKSPACE="/home/$(whoami)/jenkins-data/jenkins_home/workspace/pipeline-docker-maven"
-#WORKSPACE="/var/jenkins_home/workspace/pipeline-docker-maven"
+#WORKSPACE="/home/$(whoami)/jenkins-data/jenkins_home/workspace/pipeline-docker-maven"
+WORKSPACE="/var/jenkins_home/workspace/pipeline-docker-maven"
 echo "WORKSPACE: $WORKSPACE"
 #this is some comment but modified 
 whoami
 echo "working directory: $(pwd)"
-
+ls -la "$(pwd)"
 docker run --rm --mount type=bind,source="$WORKSPACE/java-app/",target="/app" --mount type=bind,source="/root/.m2",target="/root/.m2/" -w "/app" "grosmichelbanana/maven-alpine:v.1" "$@"
