7096a2ba334939e6df701b85a249dd7dfce00413
Krzysztof Gryko <gryko@pc-521.home>
2024-04-08 13:17:30 +0200

    workspace in mvn just like in ricardo project

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Building the code"
-#WORKSPACE="/home/$(whoami)/jenkins-data/jenkins_home/workspace/pipeline-docker-maven"
-WORKSPACE="/var/jenkins_home/workspace/pipeline-docker-maven"
+WORKSPACE="/home/$(whoami)/jenkins-data/jenkins_home/workspace/pipeline-docker-maven"
+#WORKSPACE="/var/jenkins_home/workspace/pipeline-docker-maven"
 echo "WORKSPACE: $WORKSPACE"
 #this is some comment but modified 
 whoami
