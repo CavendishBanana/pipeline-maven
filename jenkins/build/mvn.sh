50bc8fbb074e6feb507f7cc3d45ccbb31c7dc0b4
Krzysztof Gryko <gryko@pc-521.home>
2024-04-09 00:50:11 +0200

    add echo at building steps end

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -8,3 +8,4 @@ whoami
 echo "working directory: $(pwd)"
 ls -la "$(pwd)"
 docker run --rm --mount type=volume,source=jenkins-volume-pipeline-docker-maven,target=/app --mount type=bind,source=/root/.m2,target=/root/.m2/ -w /app/java-app/ grosmichelbanana/maven-alpine:v.1 ls -la
+echo "Building the code done"
