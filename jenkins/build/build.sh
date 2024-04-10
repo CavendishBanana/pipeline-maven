2256e36d9622504f1ef632897b32c57c89dee32e
Krzysztof Gryko <gryko@pc-521.home>
2024-04-10 20:19:18 +0200

    add copying jar from bounded dir to task dir

--- jenkins/build/build.sh
+++ jenkins/build/build.sh
@@ -3,7 +3,7 @@
 echo "******** Building docker image ********"
 
 cp -f /app/target/*.jar jenkins/build/
-
+cp -f /app/target/*.jar /var/jenkins_home/workspace/pipeline-docker-maven/
 cd jenkins/build/ && docker compose -f docker-compose-build.yml build --no-cache
 
 echo "******** Building docker image done *********"
