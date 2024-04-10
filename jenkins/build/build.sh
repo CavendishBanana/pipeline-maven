993c015c99af7be097aa14fb9573c0bea1d40dd3
Krzysztof Gryko <gryko@pc-521.home>
2024-04-10 18:26:58 +0200

    change binding host path in mvn

--- jenkins/build/build.sh
+++ jenkins/build/build.sh
@@ -2,6 +2,8 @@
 
 echo "******** Building docker image ********"
 
-cp -f java-app/target/*.jar jenkins/build/
+cp -f /app/java-app/target/*.jar jenkins/build/
 
 cd jenkins/build/ && docker compose -f docker-compose-build.yml build --no-cache
+
+echo "******** Building docker image done *********"
--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -7,5 +7,7 @@ echo "WORKSPACE: $WORKSPACE"
 whoami
 echo "working directory: $(pwd)"
 ls -la "$(pwd)"
-docker run --rm --mount type=volume,source=jenkins-volume-pipeline-docker-maven,target=/app --mount type=bind,source=/root/.m2,target=/root/.m2/ -w /app/java-app/ grosmichelbanana/maven-alpine:v.1 "$@"
+#docker run --rm --mount type=volume,source=jenkins-volume-pipeline-docker-maven,target=/app --mount type=bind,source=/root/.m2,target=/root/.m2/ -w /app/java-app/ grosmichelbanana/maven-alpine:v.1 "$@"
+docker run --rm --mount type=bind,source=/home/gryko/jenkins-data/pipeline/java-app,target=/app --mount type=bind,source=/root/.m2,target=/root/.m2/ -w /app/ grosmichelbanana/maven-alpine:v.1 "$@"
+
 echo "Building the code done"
